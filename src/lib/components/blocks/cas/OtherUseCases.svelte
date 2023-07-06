7ec670b684844adb222336dcb8c28cdf476cac59
Aylay <luk.tattali@gmail.com>
2023-07-06 16:31:45 +0200

    -

--- src/lib/components/blocks/cas/OtherUseCases.svelte
+++ src/lib/components/blocks/cas/OtherUseCases.svelte
@@ -12,7 +12,7 @@
   </p>
   <div class="mt-12 flex flex-col items-center gap-8">
     {#each useCases as useCase, i}
-      <Cta2 href="/cas/{useCase.slug}" label={useCase.title} {i} {isInView} />
+      <Cta2 href="/cas/{useCase.slug}" label={useCase.title} {i} />
     {/each}
   </div>
 </div>
---
+++ src/routes/jobs/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Header from "$lib/components/blocks/common/Header.svelte";
+
+  const title = "Jobs"
+  const subtitle = 'Vous pensez faire la différence ? Parlons-en !'
+</script>
+
+<div class="flex flex-col gap-48 lg:gap-96 pb-48 lg:pb-96">
+  <Header {title} {subtitle} />
+</div>
