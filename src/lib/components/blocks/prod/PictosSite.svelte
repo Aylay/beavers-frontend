4646dc5b98672d6f5def5e2c2202fb263bca5384
Aylay <luk.tattali@gmail.com>
2023-07-06 16:10:45 +0200

    -

--- src/lib/components/blocks/prod/PictosSite.svelte
+++ src/lib/components/blocks/prod/PictosSite.svelte
@@ -5,7 +5,6 @@
 	import Google from "$lib/assets/svg/Google.svelte";
 	import Medal from "$lib/assets/svg/Medal.svelte";
 	import Search2 from "$lib/assets/svg/Search2.svelte";
-	import Binoculars from "$lib/assets/svg/Binoculars.svelte";
 	import Graph from "$lib/assets/svg/Graph.svelte";
   import Shape from "$lib/assets/svg/Shape.svelte";
   import Line from "../utilities/Line.svelte";
@@ -70,7 +69,7 @@
     <div
       class="flex-1 flex flex-col gap-10 items-center animate-delay-1000 {isInView ? 'animate-fade' : 'opacity-0'}"
     >
-      <div class="relative"^>
+      <div class="relative">
         <Shape newClass="fill-seance" />
         <Google newClass="absolute top-7 left-1/2 transform -translate-x-1/2" />
       </div>
