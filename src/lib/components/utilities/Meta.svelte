0d7a43e50bbe369e90893daf5136e7a811ab59c4
Aylay <luk.tattali@gmail.com>
2023-08-07 16:10:05 +0200

    meta twitter

--- src/lib/components/utilities/Meta.svelte
+++ src/lib/components/utilities/Meta.svelte
@@ -9,12 +9,22 @@
   <title>{meta.title}</title>
   <meta name="title" content={meta.title} />
   <meta property="og:title" content={meta.title} />
+  <meta property="twitter:title" content={meta.title} />
   <meta name="description" content={meta.description} />
   <meta property="og:description" content={meta.description} />
+  <meta property="twitter:description" content={meta.description} />
 	<meta property="og:type" content="article" />
   {#if meta.ogImage && meta.ogImage.data}
     <meta
+      property="twitter:card"
+      content="summary_large_image"
+    />
+    <meta
       property="og:image"
+      content="{strapiURL}{meta.ogImage.data.attributes.url}"
+    />
+    <meta
+      property="twitter:image"
       content="{strapiURL}{meta.ogImage.data.attributes.url}"
     />
     <meta
