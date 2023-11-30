4abd90f0dd7b8dc3dc911fd48b5acfa4105d0a0e
Aylay <luk.tattali@gmail.com>
2023-11-30 14:30:27 +0100

    -

--- src/lib/components/blocks/hp/Testimonies.svelte
+++ src/lib/components/blocks/hp/Testimonies.svelte
@@ -7,10 +7,11 @@
 	import LDTag from '$lib/components/utilities/LDTag.svelte'
 
   type Testimony = {
-  name: string;
-  company: string;
-  text: string;
-};
+    name: string;
+    company: string;
+    text: string;
+    link: string;
+  };
 
 let isInView: boolean;
 const options: Options = {
@@ -26,22 +27,26 @@ const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>) => {
     {
       name: 'Fabrice Briot',
       company: 'V-ZUG',
-      text: "Une équipe très agréable, les Beavers ont fait preuve d’une très bonne écoute et d’une grande réactivité tout en étant <strong>force de proposition</strong>. Je suis ravi de cette collaboration."
+      text: "Une équipe très agréable, les Beavers ont fait preuve d’une très bonne écoute et d’une grande réactivité tout en étant <strong>force de proposition</strong>. Je suis ravi de cette collaboration.",
+      link: '/cas/creation-gestion-publications-reseaux-sociaux-v-zug'
     },
     {
       name: 'Augustin Bouyer',
       company: 'WeVan',
-      text: "Notre collaboration avec Beavers nous a permis de progresser dans l'approche des réseaux sociaux, à la fois pour la partie <strong>sponsoring des posts naturels</strong>, mais aussi et de plus en plus à travers la création de véritables <strong>campagnes de publicité sponsorisées</strong> qui ont pris de plus en plus de place dans l'écosystème digital.<br /><br />Marie-Claire nous a emmené sur de nouveaux territoires, notamment <strong>Pinterest et Linkedin</strong>, ainsi que sur de nouveaux formats. Nous avons connu pas mal de succès et quelques échecs, mais l'excellent relationnel et professionnalisme de l'équipe Beavers donne toujours envie de tester de nouvelles idées, ce qui fait en définitive l'essence du marketing digital."
+      text: "Notre collaboration avec Beavers nous a permis de progresser dans l'approche des réseaux sociaux, à la fois pour la partie <strong>sponsoring des posts naturels</strong>, mais aussi et de plus en plus à travers la création de véritables <strong>campagnes de publicité sponsorisées</strong> qui ont pris de plus en plus de place dans l'écosystème digital.<br /><br />Marie-Claire nous a emmené sur de nouveaux territoires, notamment <strong>Pinterest et Linkedin</strong>, ainsi que sur de nouveaux formats. Nous avons connu pas mal de succès et quelques échecs, mais l'excellent relationnel et professionnalisme de l'équipe Beavers donne toujours envie de tester de nouvelles idées, ce qui fait en définitive l'essence du marketing digital.",
+      link: '/cas/strategie-digitale-media-pilotage-campagnes-wevan'
     },
     {
       name: 'Nicolas Meliand',
       company: 'Frames Dealer',
-      text: "Notre collaboration au long cours avec Beavers a toujours été à la fois très agréable, fluide et efficace. Depuis l’accompagnement mené par Lucas sur des sujets d’<strong>UX</strong>, de <strong>webdesign</strong> ou encore relatifs à la <strong>conformité RGPD</strong> jusqu’au travail avec Marie-Claire sur des <strong>campagnes media très ciblées</strong>, nous avons apprécié le professionnalisme, la pédagogie et le sens de l’écoute de l’équipe.<br /><br />L’excellence qui nous porte au quotidien chez Frames Dealer a toujours trouvé un écho dans la collaboration avec l’équipe de Beavers qui a su <strong>nous challenger</strong> et mener à bien des projets ambitieux et innovants, aussi il nous tarde d’avoir de prochains projets de collaboration avec Lucas et Marie-Claire."
+      text: "Notre collaboration au long cours avec Beavers a toujours été à la fois très agréable, fluide et efficace. Depuis l’accompagnement mené par Lucas sur des sujets d’<strong>UX</strong>, de <strong>webdesign</strong> ou encore relatifs à la <strong>conformité RGPD</strong> jusqu’au travail avec Marie-Claire sur des <strong>campagnes media très ciblées</strong>, nous avons apprécié le professionnalisme, la pédagogie et le sens de l’écoute de l’équipe.<br /><br />L’excellence qui nous porte au quotidien chez Frames Dealer a toujours trouvé un écho dans la collaboration avec l’équipe de Beavers qui a su <strong>nous challenger</strong> et mener à bien des projets ambitieux et innovants, aussi il nous tarde d’avoir de prochains projets de collaboration avec Lucas et Marie-Claire.",
+      link: ''
     },
     {
       name: 'Laurent Esposito',
       company: 'Imagine 2050',
-      text: "Nous avons fait appel à l'agence Beavers pour la <strong>création complète de notre site web</strong>. Nous ne pouvons que louer la créativité, la réactivité et le professionnalisme de l'équipe.<br /><br />Nous avons notamment apprécié ses <strong>recommandations marketing et techniques</strong> pour choisir les bons outils en back-office, <strong>optimiser notre visibilité</strong> sur la toile et minimiser notre <strong>impact environnemental</strong>."
+      text: "Nous avons fait appel à l'agence Beavers pour la <strong>création complète de notre site web</strong>. Nous ne pouvons que louer la créativité, la réactivité et le professionnalisme de l'équipe.<br /><br />Nous avons notamment apprécié ses <strong>recommandations marketing et techniques</strong> pour choisir les bons outils en back-office, <strong>optimiser notre visibilité</strong> sur la toile et minimiser notre <strong>impact environnemental</strong>.",
+      link: '/cas/developpement-site-internet-imagine-2050'
     }
   ]
   
@@ -107,7 +112,17 @@ on:inview_change={handleChange}>
         { testimony.name }
       </p>
       <p class="text-6 font-bold text-seance">
+        {#if testimony.link !== ''}
+          <a
+            href={testimony.link}
+            title={testimony.company}
+            class="underline hover:no-underline"
+          >
+            { testimony.company }
+          </a>
+        {:else}
         { testimony.company }
+        {/if}
       </p>
     </div>
     {/each}
