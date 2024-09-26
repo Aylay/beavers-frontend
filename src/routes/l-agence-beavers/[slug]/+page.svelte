bb7b619569265f1f0045ad0631b7ba6c64d058fb
Aylay <luk.tattali@gmail.com>
2024-09-26 18:09:57 +0200

    useCases in agencies

--- src/routes/l-agence-beavers/[slug]/+page.svelte
+++ src/routes/l-agence-beavers/[slug]/+page.svelte
@@ -4,6 +4,7 @@
 	import Meta from '$lib/components/utilities/Meta.svelte';
   import Header from "$lib/components/blocks/agence/page/Header.svelte";
   import ContentManager from '$lib/components/blocks/common/ContentManager.svelte';
+	import UseCases from '$lib/components/blocks/UseCases.svelte';
 
   let content: any = $page.data.content.attributes;
   
@@ -18,5 +19,8 @@
 	<Header title={content.title} subtitle={content.subTitle} intro={content.intro} words={content.words} />
   {#if content.contentManager}
 	<ContentManager content={content.contentManager} title={content.title} />
+	{/if}
+	{#if content.useCases && content.useCases.data && content.useCases.data.length > 0}
+	<UseCases cases={content.useCases.data} firstText="Quelques" />
 	{/if}
 </div>
