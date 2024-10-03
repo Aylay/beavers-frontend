c5d3410e075554fd91e0242ea41c9fadf87d885e
Aylay <luk.tattali@gmail.com>
2024-10-03 16:54:44 +0200

    -

--- src/routes/l-agence-beavers/+page.svelte
+++ src/routes/l-agence-beavers/+page.svelte
@@ -21,42 +21,42 @@
     {
       img : '/signature/beavers-marie-claire.png',
       webp : '/signature/beavers-marie-claire.webp',
-      name: 'Marie-Claire BLAIZOT',
+      name: 'Marie-Claire Blaizot',
       job: 'Directrice Générale & Directrice Média',
       text: "Marie-Claire vous propose son expertise en stratégie et activation média online, après 10 ans en agence média et chez l’annonceur.<br /><br /><span class='text-electric font-semibold'>Au menu, le matin :</span> un grand bol de plans média,<br /><span class='text-electric font-semibold'>Le midi :</span> salade de pixels d’activation (à consommer sans modération)<br /><span class='text-electric font-semibold'>Le soir :</span> farandole de bilans média (Excel est son ami)"
     },
     {
       img : '/signature/beavers-lucas.png',
       webp : '/signature/beavers-lucas.webp',
-      name: 'Lucas ATTALI',
+      name: 'Lucas Attali',
       job: 'Président & Directeur de Production',
       text: "Ses expériences en tant que Développeur Front-End, Product Owner, Chef de Projets et Responsable de Production, lui ont appris à penser un projet dans sa globalité sans en segmenter chaque phase. Aujourd'hui, Lucas aime travailler dans un univers fun mais toujours très bien cadré ; avec lui pas de bannière de cookies, pas de site <span class='text-electric font-semibold'>#RGPDMaster</span> !!!"
     },
     {
       img : '/signature/beavers-orane.png',
       webp : '/signature/beavers-orane.webp',
-      name: 'Orane DEMANDRE',
+      name: 'Orane Demandre',
       job: 'Cheffe de projets Média',
       text: "Après avoir travaillé en tant que chargée de marketing et communication et avec une expérience approfondie en relation client, Orane sera très soucieuse de votre satisfaction.<br /><br />Du haut de ses 162 cm, elle sera prête à prendre des échasses pour <span class='text-electric font-semibold'>vous positionner au plus haut</span> !"
     },
     {
       img : '/img/agence/claire.png',
       webp : '/img/agence/claire.webp',
-      name: 'Claire MÉJEAN',
+      name: 'Claire Méjean',
       job: 'UX / UI Designer',
       text: "Passionnée d'art et d'informatique depuis toujours, Claire vous accompagne en tant qu'UX / UI Designer dans le cadre du développement de vos projets numériques.<br /><br /><span class='text-electric font-semibold'>Autodidacte, touche à tout</span>, elle met à votre profit son expérience de plus de 11 ans !"
     },
     {
       img : '/img/agence/virginia.png',
       webp : '/img/agence/virginia.webp',
-      name: 'Virginia BLANCO',
+      name: 'Virginia Blanco',
       job: 'Rédactrice web SEO',
       text: "Amoureuse des mots, Virginia crée du contenu unique et pertinent pour propulser votre site internet. En tant que rédactrice web SEO bilingue français-espagnol et correctrice, elle s'adapte à tout projet digital dans les deux langues. Elle vous accompagne dans la mise en place d'une stratégie éditoriale afin d'obtenir le meilleur référencement possible.<br /><br /><span class='text-electric font-semibold'>Sa potion magique :</span> une écriture fluide adaptée au web associée aux techniques SEO !"
     },
     {
       img : '/img/agence/theo.png',
       webp : '/img/agence/theo.webp',
-      name: 'Théo SALOMONI',
+      name: 'Théo Salomoni',
       job: 'Chef de projets digitaux',
       text: "Karine vous propose son expertise en community management. Elle vous aidera à exploiter la puissance des réseaux pour développer votre présence digitale  🚀.<br /><br />Ultra-connectée, ses missions sont une vraie passion et les réseaux son <span class='text-electric font-semibold'>terrain de jeu</span> !"
     }
